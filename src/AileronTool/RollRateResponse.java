3bf3b127426008c62150e3b5d2ae0998596fd8b1
Daniel Levy <u6996577@anu.edu.au>
2019-07-14 19:28:19 +1000

    Aileron Tool Grid Setup

--- src/AileronTool/RollRateResponse.java
+++ src/AileronTool/RollRateResponse.java
@@ -42,15 +42,49 @@ public class RollRateResponse {
 
     public static boolean verify (double y_o, double y_i, double CCa, double delta_A) {
         double tau = tau(CCa);
+        // System.out.println(tau+" tau");
         double C_ldA = C_ldA(y_o, y_i, tau);
+        // System.out.println(C_ldA+" clda");
         double C_l = C_l(C_ldA, delta_A);
+        // System.out.println(C_l+" C_l");
         double L_A = L_A(C_l);
+        // System.out.println(L_A+" L_A");
         double P_ss = P_ss(L_A);
-        
-        /*if () {
+        // System.out.println(P_ss+" P_ss");
+        double phi_1 = phi_1(P_ss);
+        // System.out.println(phi_1+" phi_1");
+        double P_dot = P_dot(P_ss, phi_1);
+        // System.out.println(P_dot+" P_dot");
 
-        }*/
-        return false;
+        if (phi_1 > 30*Math.PI/180) {
+            // System.out.println("a");
+            // System.out.println(t_ss(30*Math.PI/180, P_dot));
+            return (t_ss(30*Math.PI/180, P_dot) > 2.25 && t_ss(30*Math.PI/180, P_dot) < 2.75);
+        } else {
+            // System.out.println("b");
+            // System.out.println(t_ss(phi_1, P_dot));
+            // System.out.println(t_req(phi_1, P_ss, t_ss(phi_1, P_dot)));
+            return (t_req(phi_1, P_ss, t_ss(phi_1, P_dot)) > 2.25 && t_req(phi_1, P_ss, t_ss(phi_1, P_dot)) < 2.75);
+        }
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(verify(61.5,50.1, 0.22, 25*Math.PI/180));
+        for (int a=5; a < 31; a=a+5) {
+           for (int b = 5; b < 31; b++) {
+              for (int c = 20; c < 62; c++) {
+                  for (int d = 20; d < c; d++) {
+                      double da = (double)a;
+                      double db = (double)b/100;
+                      double dc = (double)c;
+                      double dd = (double)d;
+                      if (verify(dc, dd, db, da)) {
+
+                      }
+                  }
+              }
+           }
+        }
     }
 
 
